d8de37311bcc746c87a7666021fa11ee702ef49a
3ichael 7ambert <mlambertbusiness@gmail.com>
2023-04-11 19:03:29 -0600

    commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let result = document.getElementById('result');
+
+function addToResult(value) {
+  result.value += value;
+}
+
+function clearResult() {
+  result.value = '';
+}
+
+function deleteLast() {
+  result.value = result.value.slice(0, -1);
+}
+
+function calculate() {
+  result.value = eval(result.value);
+}
+
+function squareRoot() {
+  result.value = Math.sqrt(result.value);
+}
